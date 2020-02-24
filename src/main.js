12c714d5a8be46609abb937a7b79996534cddccc
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-24 23:03:29 +0300

    removed shit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+
+import { router, store, vuetify } from '@/plugins';
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
