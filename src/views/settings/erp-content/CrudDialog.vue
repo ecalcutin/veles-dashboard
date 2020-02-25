bdb9034d234305bf86f76ebadf52753e0209bbd0
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-26 01:39:20 +0300

    creating crud dialog component

---
+++ src/views/settings/erp-content/CrudDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-dialog @click:outside="close('cancel')" :value="opened" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span v-if="mode === 'create'">Создать запись</span>
+          <span v-if="mode === 'update'">Редактировать</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            {{mode}}
+            <slot></slot>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="red" v-if="mode==='update'" @click="close('remove')" text>Удалить</v-btn>
+          <v-spacer />
+          <v-btn @click="close('cancel')" text>Отмена</v-btn>
+          <v-btn color="green" v-if="mode === 'create'" @click.stop="close('create')" text>Создать</v-btn>
+          <v-btn color="green" v-if="mode==='update'" @click="close('update')" text>Обновить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CrudDialog",
+  props: {
+    mode: {
+      type: String,
+      required: true
+    },
+    opened: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    close(mode) {
+      this.$emit("onClose", mode);
+    }
+  }
+};
+</script>
