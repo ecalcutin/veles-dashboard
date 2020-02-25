ea310d757a945a05237e00911122e21c6429036e
Evgheni Calcutin <42963334+ecalcutin@users.noreply.github.com>
2020-02-26 01:39:58 +0300

    using crud dialog

--- src/views/settings/erp-content/Products.vue
+++ src/views/settings/erp-content/Products.vue
@@ -2,7 +2,14 @@
   <div>
     <v-card>
       <v-toolbar flat>
-        <v-btn text>Добавить</v-btn>
+        <v-btn @click.stop="openCrudDialog('create', defaultItem)" text>Создать</v-btn>
+        <CrudDialog @onClose="onClose" :mode="crud.mode" :opened="crud.opened">
+          <template>
+            <v-form>
+              <v-text-field label="UUID" v-model="defaultItem._id" />
+            </v-form>
+          </template>
+        </CrudDialog>
       </v-toolbar>
       <v-data-table
         :headers="headers"
@@ -10,12 +17,26 @@
         :items-per-page.sync="itemsPerPage"
         :page.sync="page"
         :items="items"
-      ></v-data-table>
+      >
+        <template v-slot:item="row">
+          <tr>
+            <td>{{row.item._id}}</td>
+            <td>{{row.item.title}}</td>
+            <td>{{row.item.imageURI}}</td>
+            <td>
+              <v-btn @click.stop="openCrudDialog('update', row.item)" icon small>
+                <v-icon dark>mdi-pencil-outline</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
     </v-card>
   </div>
 </template>
 
 <script>
+import CrudDialog from "./CrudDialog";
 import { PRODUCTS_GET } from "@/store/settings/action-types";
 import {
   PAGE_LIMIT_SET,
@@ -23,19 +44,60 @@ import {
 } from "@/store/settings/mutation-types";
 export default {
   name: "Products",
+  components: {
+    CrudDialog
+  },
   data() {
     return {
+      crud: {
+        opened: true,
+        mode: "create"
+      },
+      defaultItem: {
+        _id: "",
+        title: "",
+        imageURI: ""
+      },
       headers: [
         { text: "UUID", value: "_id", sortable: false },
         { text: "Артикул", value: "title", sortable: false },
-        { text: "Image URI", value: "imageURI", sortable: false }
+        { text: "Image URI", value: "imageURI", sortable: false },
+        { text: "Действия", sortable: false }
       ]
     };
   },
   mounted() {
     this.$store.dispatch(PRODUCTS_GET);
   },
-  methods: {},
+  methods: {
+    openCrudDialog(mode, item) {
+      this.defaultItem = Object.assign({}, item);
+      this.crud.mode = mode;
+      this.crud.opened = true;
+    },
+    onClose(mode) {
+      switch (mode) {
+        case "create":
+          console.log("Creating: ", this.defaultItem);
+          break;
+        case "update":
+          console.log("Updating: ", this.defaultItem);
+          break;
+        case "remove":
+          console.log("Removing");
+          break;
+        case "cancel":
+          console.log("Cancelling");
+          break;
+      }
+      this.crud.opened = false;
+      this.defaultItem = {
+        _id: "",
+        title: "",
+        imageURI: ""
+      };
+    }
+  },
   computed: {
     items() {
       return this.$store.state.settings.products.items;
